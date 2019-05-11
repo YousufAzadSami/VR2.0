26f09286cef47da309e40733bfe4ad43cc705f90
Yousuf Azad <sami1592@gmail.com>
2019-05-11 12:20:51 +0200

    alpha channel change and animation on it works on cube

---
+++ AnimationFadeOut/Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public float alphaValue;
+    public Color colorA;
+    public Material materialA;
+
+	// Use this for initialization
+	void Start () {
+        materialA = GetComponent<Renderer>().material;
+        colorA = materialA.color;
+        alphaValue = materialA.color.a;        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        colorA.a = alphaValue;
+        materialA.color = colorA;
+	}
+}
