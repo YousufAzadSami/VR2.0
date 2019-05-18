f8ea5a161ff07592229fd118f9cf36352b3e88af
Yousuf Azad <sami1592@gmail.com>
2019-05-18 13:10:03 +0200

    doing transition to alpha with color, instead of changing alpha color values

--- AnimationFadeOut/Assets/Scripts/Alpha.cs
+++ AnimationFadeOut/Assets/Scripts/Alpha.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Alpha : MonoBehaviour {
 
-    public float alphaValue = 1;
-    private Color colorA;
+    //public float alphaValue = 1;
+    //private Color colorA;
     private Material materialA;
+
+    public Color colorValue;
+    //public Color oldColor;
 
 	// Use this for initialization
 	void Start () {
         materialA = GetComponent<Renderer>().material;
-        colorA = materialA.color;
-        alphaValue = materialA.color.a;        
+        //colorA = materialA.color;
+        //alphaValue = materialA.color.a;
+
+        colorValue = materialA.color;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        colorA.a = alphaValue;
-        materialA.color = colorA;
+        //colorA.a = alphaValue;
+        //materialA.color = colorA;
+
+        materialA.color = colorValue;
 	}
 }
